2db90eca2caa669c4fdf60de41f176e3bb5711a2
baluchowdary <bhardwaja.kollu@gmail.com>
2024-04-17 01:02:04 -0400

    Oracle DB Conn

--- src/main/java/com/kollu/order/entity/Order.java
+++ src/main/java/com/kollu/order/entity/Order.java
@@ -3,6 +3,7 @@ package com.kollu.order.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,7 +13,7 @@ import lombok.Setter;
 @Entity(name = "Order_Table")
 public class Order {
 	@Id
-	@GeneratedValue	
+	@GeneratedValue(strategy = GenerationType.AUTO)	
 	private long id;
 	
 	@Column(name = "Order_ITEM")
